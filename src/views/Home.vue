fe324cc466b8f168f52e2926e80f2a207db89307
Mohamed Donzo <demahom18@outlook.com>
2021-02-16 13:01:18 -0500

    feat: add features sceion  + responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,31 +4,25 @@
     <SectionStory/>
     <SectionDesign/>
     <StoryList/>
+    <FeaturesSection />
   </div>
 </template>
 
 <script>
 
+import FeaturesSection from '../components/FeaturesSection.vue'
 import SectionDesign from '../components/SectionDesign.vue'
-import SectionHero from '../components/SectionHero.vue'
 import SectionStory from '../components/SectionStory.vue'
+import SectionHero from '../components/SectionHero.vue'
 import StoryList from '../components/StoryList.vue'
 export default {
   name: 'Home',
   components: { 
+    FeaturesSection,
     SectionDesign, 
-    SectionHero, 
     SectionStory, 
+    SectionHero, 
     StoryList
-  },
-  setup() {
-    const blockContent = {
-      title: 'Create and share your photo stories.',
-      resume: 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
-      alt: 'Man on a bridge`'
-    }
-
-    return { blockContent }
   }
 }
 </script>
